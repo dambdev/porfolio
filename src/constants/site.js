9a8ff383960909a745dc63209a0b0fb4ea3535fe
dambdev <david_miras@icloud.com>
2025-05-08 21:01:19 +0200

    Agrega un archivo manifest.json para mejorar la experiencia del usuario y la instalación de la aplicación web. Se actualizan las constantes del sitio para incluir la URL y el dominio, y se modifican los metadatos en el layout para utilizar estas nuevas constantes, optimizando así la configuración de SEO y la presentación del sitio.

--- src/constants/site.js
+++ src/constants/site.js
@@ -3,5 +3,7 @@ export const SITE = {
   description:
     'Desarrollador web Full Stack especializado en crear aplicaciones web modernas, responsivas y de alto rendimiento. Descubre mis proyectos y habilidades técnicas.',
   keywords:
-    'desarrollo web, full stack, frontend, backend, HTML, CSS, Tailwind, JavaScript, TypeScript, MySQL, MongoDB, Node.js, React, Next.js, Gatsby, Astro, PHP, Laravel, portfolio, David Miras, dambdev, proyectos web, aplicaciones web'
+    'desarrollo web, full stack, frontend, backend, HTML, CSS, Tailwind, JavaScript, TypeScript, MySQL, MongoDB, Node.js, React, Next.js, Gatsby, Astro, PHP, Laravel, portfolio, David Miras, dambdev, proyectos web, aplicaciones web',
+  url: 'https://damb.dev',
+  domain: 'damb.dev'
 }
