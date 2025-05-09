051c9071a687543684690413043e0733740d25ef
dambdev <david_miras@icloud.com>
2025-05-09 21:25:04 +0200

    Actualiza la configuración de Astro para incluir la integración de astro-robots-txt y se establece la política de robots. Además, se mejora el componente Header.astro al agregar etiquetas aria para accesibilidad en los botones de cambio de tema. Se añade la dependencia de astro-robots-txt en package.json y se actualiza el archivo pnpm-lock.yaml.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,27 @@
-// @ts-check
-import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
 import tailwindcss from '@tailwindcss/vite'
-import react from '@astrojs/react';
-// https://astro.build/config
+import robotsTxt from 'astro-robots-txt'
+import { defineConfig } from 'astro/config'
+
 export default defineConfig({
+  site: 'https://damb.dev',
+
+  trailingSlash: 'always',
+
+  integrations: [
+    react(),
+    robotsTxt({
+      host: 'https://damb.dev',
+      policy: [
+        {
+          userAgent: '*',
+          allow: '/'
+        }
+      ]
+    })
+  ],
+
   vite: {
     plugins: [tailwindcss()]
-  },
-
-  integrations: [react()]
+  }
 })
