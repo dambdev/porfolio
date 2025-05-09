1a319df82f1b394922fde2a0894630176dbd854a
dambdev <david_miras@icloud.com>
2025-05-09 17:25:39 +0200

    Agrega nuevos componentes: Badge, Hero, SectionContainer y SocialPill. Se implementa la estructura de la página principal con un contenedor de secciones y se define la navegación del sitio mediante un archivo de constantes. Estos cambios mejoran la presentación y la funcionalidad del sitio web.

---
+++ src/constants/navigation.js
@@ -0,0 +1,27 @@
+export const navItems = [
+  {
+    title: 'Inicio',
+    label: 'inicio',
+    url: '/#inicio'
+  },
+  {
+    title: 'Experiencia',
+    label: 'experiencia',
+    url: '/#experiencia'
+  },
+  {
+    title: 'Proyectos',
+    label: 'proyectos',
+    url: '/#proyectos'
+  },
+  {
+    title: 'Sobre mí',
+    label: 'sobre-mi',
+    url: '/#sobre-mi'
+  },
+  {
+    title: 'Contacto',
+    label: 'contacto',
+    url: '/#contacto'
+  }
+]
