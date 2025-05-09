7ab93431a1bc29fef5f520ccf1b50471f7776a72
dambdev <david_miras@icloud.com>
2025-05-09 18:28:28 +0200

    Agrega nuevos componentes de experiencia y título, junto con un ícono de maletín. Se implementa la sección de experiencia técnica en la página principal, mejorando la presentación de la trayectoria profesional.

---
+++ src/constants/experience.js
@@ -0,0 +1,21 @@
+export const EXPERIENCE = [
+  {
+    date: 'Marzo 2025 - Abril 2025',
+    title: 'Desarrollador Web Full Stack',
+    company: 'Freelance',
+    description:
+      'Aplicación desarrollada desde cero y desplegada en mi propio VPS, optimizada para rendimiento y seguridad a través del CDN de Cloudflare.'
+  },
+  {
+    date: 'Agosto - Noviembre 2024',
+    title: 'Desarrollador Web Full Stack',
+    company: 'Hack a Boss',
+    description: 'Aplicación desarrollada desde cero para el proyecto final del bootcamp.'
+  },
+  {
+    date: 'Mayo 2024',
+    title: 'Desarrollador Web',
+    company: 'Hack a Boss',
+    description: 'Desarrollo de mini aplicación web de mitad de bootcamp.'
+  }
+]
