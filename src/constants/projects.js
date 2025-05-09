e32c880faed6fbadf6b24696358ca665f0780d9b
dambdev <david_miras@icloud.com>
2025-05-09 19:47:57 +0200

    Agrega nuevos componentes: LinkButton y Projects, junto con un archivo de constantes para gestionar la información de los proyectos. Se implementa la sección de proyectos en la página principal, mejorando la presentación y accesibilidad de los mismos.

---
+++ src/constants/projects.js
@@ -0,0 +1,71 @@
+import ceipOutesImg from '../assets/projects/ceipoutes.webp'
+import clockYouImg from '../assets/projects/clockyou.webp'
+import ticTacToeImg from '../assets/projects/tictactoe.webp'
+import timelineImg from '../assets/projects/timeline.webp'
+
+import CSSIcon from '../icons/CSS.astro'
+import HTMLIcon from '../icons/HTML.astro'
+import JSIcon from '../icons/JS.astro'
+import MySQLIcon from '../icons/MySQL.astro'
+import NodeJSIcon from '../icons/NodeJS.astro'
+import ReactIcon from '../icons/React.astro'
+import TailwindIcon from '../icons/Tailwind.astro'
+
+const SITES_URLS = {
+  ceipOutes: 'https://comedor.ceipoutes.org',
+  clockYou: 'https://clockyou.damb.dev',
+  ticTacToe: 'https://tictactoe.damb.dev',
+  timeline: 'https://linea-temporal.netlify.app'
+}
+
+const GITHUB_URLS = {
+  clockYou: 'https://github.com/dambdev/clock-you',
+  timeline: 'https://github.com/dambdev/timeline'
+}
+
+export const PROJECTS = [
+  {
+    title: 'Ceip Outes',
+    description:
+      'Un proyecto diseñado para que las familias puedan registrar la asistencia de sus hijos al comedor y a las actividades extraescolares, mientras el colegio gestiona y hace seguimiento de todo de forma digital y eficiente.',
+    link: SITES_URLS.ceipOutes,
+    image: ceipOutesImg,
+    tags: [
+      { icon: NodeJSIcon },
+      { icon: MySQLIcon },
+      { icon: JSIcon },
+      { icon: ReactIcon },
+      { icon: TailwindIcon }
+    ]
+  },
+  {
+    title: 'ClockYou',
+    description:
+      'Aplicación para búsqueda y contratación de servicios personalizados. Permite a las empresas gestionar su personal de manera centralizada y ofrece una solución digital de recursos humanos.',
+    link: SITES_URLS.clockYou,
+    github: GITHUB_URLS.clockYou,
+    image: clockYouImg,
+    tags: [
+      { icon: NodeJSIcon },
+      { icon: MySQLIcon },
+      { icon: JSIcon },
+      { icon: ReactIcon },
+      { icon: TailwindIcon }
+    ]
+  },
+  {
+    title: 'Tic Tac Toe',
+    description: 'Clásico juego Tic Tac Toe en el que jugarás contra la IA.',
+    link: SITES_URLS.ticTacToe,
+    image: ticTacToeImg,
+    tags: [{ icon: ReactIcon }, { icon: JSIcon }, { icon: TailwindIcon }]
+  },
+  {
+    title: 'Timeline',
+    description: 'Aplicación que transforma un JSON con eventos en una línea temporal visual.',
+    link: SITES_URLS.timeline,
+    github: GITHUB_URLS.timeline,
+    image: timelineImg,
+    tags: [{ icon: HTMLIcon }, { icon: CSSIcon }, { icon: JSIcon }]
+  }
+]
