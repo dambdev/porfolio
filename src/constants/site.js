a3ff165fb276b79edafbf8e46aa1001b1452d0d3
dambdev <david_miras@icloud.com>
2025-05-12 17:12:53 +0200

    Actualiza la información personal en varios archivos, cambiando "dambdev" por "David Mirás Bellón (dambdev)" en el LICENSE, manifest.json, Hero.astro, y site.js, mejorando la presentación y consistencia del autor en el proyecto.

--- src/constants/site.js
+++ src/constants/site.js
@@ -1,9 +1,9 @@
 export const SITE = {
-  title: 'David Mirás (dambdev) | Desarrollador Full Stack',
+  title: 'David Mirás Bellón (dambdev) | Desarrollador Full Stack',
   description:
     'Desarrollador Full Stack especializado en React y Astro. Creo soluciones web modernas, rápidas y personalizadas. Explora mi portfolio de proyectos.',
   keywords:
-    'desarrollo web, full stack, frontend, backend, HTML, CSS, Tailwind, JavaScript, TypeScript, MySQL, MongoDB, Node.js, React, Next.js, Gatsby, Astro, PHP, Laravel, portfolio, David Miras, dambdev, proyectos web, aplicaciones web',
+    'desarrollo web, full stack, frontend, backend, HTML, CSS, Tailwind, JavaScript, TypeScript, MySQL, MongoDB, Node.js, React, Next.js, Gatsby, Astro, PHP, Laravel, portfolio, David Mirás Bellón, dambdev, proyectos web, aplicaciones web',
   url: 'https://damb.dev',
   domain: 'damb.dev'
 }
