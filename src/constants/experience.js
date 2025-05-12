790253c9e70301cb5c182fd6ed08d42314c3b251
dambdev <david_miras@icloud.com>
2025-05-12 21:39:00 +0200

    Refactoriza el componente Experience.astro para mejorar la presentación y la semántica del contenido, ajustando la estructura del encabezado y la sección de descripción. Además, se actualizan las descripciones en experience.js para eliminar saltos de línea innecesarios, mejorando la legibilidad del texto.

--- src/constants/experience.js
+++ src/constants/experience.js
@@ -3,8 +3,7 @@ export const EXPERIENCE = [
     date: 'Marzo 2025 - Abril 2025',
     title: 'Desarrollador Full Stack',
     company: 'Freelance',
-    description: `
-      Desarrollé una aplicación web full stack desde cero utilizando React en el frontend y Node.js junto a MySQL en el backend.
+    description: `Desarrollé una aplicación web full stack desde cero utilizando React en el frontend y Node.js junto a MySQL en el backend.
       Implementé autenticación segura con JWT, diseñé una arquitectura escalable y desplegué la solución en un servidor VPS configurado con Nginx y PM2.
       Además, optimicé el rendimiento mediante estrategias de caché y compresión, y reforcé la seguridad con Cloudflare integrando protección DDoS y reglas WAF personalizadas.`
   },
@@ -12,8 +11,7 @@ export const EXPERIENCE = [
     date: 'Agosto - Noviembre 2024',
     title: 'Prácticas Full Stack',
     company: 'Hack a Boss',
-    description: `
-      Colaboré en un equipo ágil para desarrollar una aplicación web completa como proyecto final del bootcamp.
+    description: `Colaboré en un equipo ágil para desarrollar una aplicación web completa como proyecto final del bootcamp.
       Implementé funcionalidades del frontend con React y componentes reutilizables, mientras gestioné el backend con Node.js y Express.
       Diseñamos una base de datos relacional en MySQL, implementamos autenticación JWT.
       Esta experiencia consolidó mis habilidades técnicas y de trabajo en equipo en un entorno profesional simulado.
@@ -23,8 +21,7 @@ export const EXPERIENCE = [
     date: 'Mayo 2024',
     title: 'Prácticas Frontend',
     company: 'Hack a Boss',
-    description: `
-      Desarrollamos una web como proyecto de medio término durante el bootcamp.
+    description: `Desarrollamos una web como proyecto de medio término durante el bootcamp.
       Implementamos interfaces responsivas utilizando HTML5, CSS3 y JavaScript ES6, con enfoque en principios de diseño UI/UX.
       Este proyecto me permitió afianzar conocimientos fundamentales en desarrollo frontend y mejorar mis habilidades de resolución de problemas.`
   }
